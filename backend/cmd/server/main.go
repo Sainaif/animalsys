09b3f537a3500b1cbd964ce017aa8d4b2b66f531
Sainaif <Dawid.Rubacha@syssecure.pl>
2025-11-17 18:10:11 +0100

    Update CORS_ALLOWED_ORIGINS in docker-compose.prod.yml to allow all origins, modify corsMiddleware in main.go to handle CORS headers correctly

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -439,15 +439,20 @@ func corsMiddleware(cfg *config.Config) gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			if allowAll || containsOrigin(allowedOrigins, origin) {
+
+		if allowAll {
+			if origin != "" {
 				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			} else {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			}
-		} else if allowAll {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			// When allowing all origins, omit credentials to satisfy browsers
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
+		} else if origin != "" && containsOrigin(allowedOrigins, origin) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
 
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
